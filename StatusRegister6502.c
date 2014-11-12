e6adf515d03fef4cb318e64303ff5bbd1d6a0789
Alex <yanalex981@hotmail.com>
2014-11-12 02:36:01 -0500

    Forgot to add StatusRegister

---
+++ StatusRegister6502.c
@@ -0,0 +1,138 @@
+#include <stdint.h>
+#include "StatusRegister6502.h"
+
+#define NEGATIVE	0x80
+#define OVERFLOW	0x40
+#define DUD		0x20
+#define BREAK		0x10
+#define DECIMAL		0x08
+#define IRQ		0x04
+#define ZERO		0x02
+#define CARRY		0x01
+
+typedef struct StatusRegister6502
+{
+	uint8_t status;
+} StatusRegister6502;
+
+void initStatus6502(StatusRegister6502* self)
+{
+	self->status = DUD;
+}
+
+uint8_t setStatusByte(const StatusRegister6502* self, uint8_t status)
+{
+	self->status = status | DUD;
+}
+
+uint8_t getStatusByte(const StatusRegister6502* self)
+{
+	return self->status;
+}
+
+// GETTER
+bool isNegative(const StatusRegister6502* self)
+{
+	return (self->status & NEGATIVE)? true : false;
+}
+
+bool overflowing(const StatusRegister6502* self)
+{
+	return (self->status & NEGATIVE)? true : false;
+}
+
+bool breaking(const StatusRegister6502* self)
+{
+	return (self->status & NEGATIVE)? true : false;
+}
+
+bool inDecimalMode(const StatusRegister6502* self)
+{
+	return (self->status & NEGATIVE)? true : false;
+}
+
+bool isIRQdisabled(const StatusRegister6502* self)
+{
+	return (self->status & NEGATIVE)? true : false;
+}
+
+bool isZero(const StatusRegister6502* self)
+{
+	return (self->status & NEGATIVE)? true : false;
+}
+
+bool isCarrying(const StatusRegister6502* self)
+{
+	return (self->status & NEGATIVE)? true : false;
+}
+// SETTER
+void setNegative(StatusRegister6502* self)
+{
+	self->status = self->status | NEGATIVE;
+}
+
+void setOverflow(StatusRegister6502* self)
+{
+	self->status = self->status | OVERFLOW;
+}
+
+void setBreak(StatusRegister6502* self)
+{
+	self->status = self->status | BREAK;
+}
+
+void setDecimalMode(StatusRegister6502* self)
+{
+	self->status = self->status | DECIMAL;
+}
+
+void setIRQDisabled(StatusRegister6502* self)
+{
+	self->status = self->status | IRQ;
+}
+
+void setZero(StatusRegister6502* self)
+{
+	self->status = self->status | ZERO;
+}
+
+void setCarry(StatusRegister6502* self)
+{
+	self->status = self->status | CARRY;
+}
+
+// CLEARER
+void clearNegative(StatusRegister6502* self)
+{
+	self->status = self->status & ~NEGATIVE;
+}
+
+void clearOverflow(StatusRegister6502* self)
+{
+	self->status = self->status & ~OVERFLOW;
+}
+
+void clearBreak(StatusRegister6502* self)
+{
+	self->status = self->status & ~BREAK;
+}
+
+void clearDecimalMode(StatusRegister6502* self)
+{
+	self->status = self->status & ~DECIMAL;
+}
+
+void clearIRQ(StatusRegister6502* self)
+{
+	self->status = self->status & ~IRQ;
+}
+
+void clearZero(StatusRegister6502* self)
+{
+	self->status = self->status & ~ZERO;
+}
+
+void clearCarry(StatusRegister6502* self)
+{
+	self->status = self->status & ~CARRY;
+}
