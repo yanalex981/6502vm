2a92d95bfd823f28f1b61843a6889362dcffd3d5
Alex <yanalex981@hotmail.com>
2014-11-13 00:26:34 -0500

    Finished Memory16.c, and fixed some typos in Memory16.h

---
+++ Memory16.c
@@ -0,0 +1,89 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Memory16.h"
+
+typedef struct
+{
+	uint8_t* bytes;
+	uint16_t size;
+} Memory16;
+
+Memory16* constructMemory(Memory16* self, uint16_t size)
+{
+	self->bytes = malloc(size * sizeof(uint8_t));
+	self->size = size;
+}
+
+void destructMemory(Memory16* self)
+{
+	free(self->bytes);
+}
+
+uint8_t getByteAt(Memory16* self, uint16_t address)
+{
+	if (address > self->size)
+		return 0;
+	
+	return self->bytes[address];
+}
+
+void setByteAt(Memory16* self, uint16_t address, uint8_t value)
+{
+	if (address < size)
+		self->bytes[address] = value;
+}
+
+uint16_t getLEWordAt(Memory16* self, uint16_t address)
+{
+	uint16_t word = getByteAt(self, address + 1);
+	word <<= 8;
+	word |= getByteAt(self, address);
+
+	return word;
+}
+
+void setLEWordAt(Memory16* self, uint16_t address, uint16_t value)
+{
+	uint8_t low = value & 0x00FF;
+	uint8_t high = (value & 0xFF00) >> 8;
+
+	setByteAt(self, address, low);
+	setByteAt(self, address + 1, high);
+}
+
+uint16_t getBEWordAt(Memory16* self, uint16_t address)
+{
+	uint16_t word = getByteAt(address);
+	word <<= 8;
+	word |= getByteAt(self, address + 1);
+
+	return word;
+}
+
+void setBEWordAt(Memory16* self, uint16_t address, uint16_t value)
+{
+	uint8_t low = value & 0x00FF;
+	uint8_t high = (value & 0xFF00) >> 8;
+
+	setByteAt(self, address, high);
+	setByteAt(self, address + 1, low);
+}
+
+void dumpi(Memory16* self, FILE* out, size_t width)
+{
+	for (int i = 0; i < self->size; ++i) {
+		printf("%u", self->bytes[i]);
+
+		if (i % width != 0)
+			printf(" ");
+		else
+			printf("\n");
+	}
+}
+
+void dump(Memory16* self, FILE* out)
+{
+	dumpi(self, out, 10);
+}
--- include/Memory16.h
+++ include/Memory16.h
@@ -13,13 +13,13 @@ void destructMemory(Memory16** self);
 uint8_t getByteAt(Memory16* self, uint16_t address);
 void setByteAt(Memory16* self, uint16_t address, uint8_t value);
 
-uint16_t getLEDWordAt(Memory16* self, uint16_t address);
-void setLEDWordAt(Memory16* self, uint16_t address, uint16_t value);
+uint16_t getLEWordAt(Memory16* self, uint16_t address);
+void setLEWordAt(Memory16* self, uint16_t address, uint16_t value);
 
-uint16_t getBEDWordAt(Memory16* self, uint16_t address);
-void setBEDWordAt(Memory16* self, uint16_t address, uint8_t value);
+uint16_t getBEWordAt(Memory16* self, uint16_t address);
+void setBEWordAt(Memory16* self, uint16_t address, uint16_t value);
 
-void dump(Memory16* self, FILE* out);
 void dumpi(Memory16* self, FILE* out, size_t width);
+void dump(Memory16* self, FILE* out);
 
 #endif
